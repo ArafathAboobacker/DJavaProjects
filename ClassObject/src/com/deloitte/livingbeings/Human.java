4fde498da8c492e5a09ae5b53ad4d1a445179aac
huhy <huhy@huhy-master-node>
2018-07-24 10:24:03 +0530

    Initial Commit

---
+++ ClassObject/src/com/deloitte/livingbeings/Human.java
@@ -0,0 +1,17 @@
+package com.deloitte.livingbeings;
+
+import com.deloitte.livingbeings.organs.Eye;
+
+public class Human extends Mammals {
+	public HumanEye leftEye,rightEye;//fields
+
+	public Human(){
+		leftEye = new HumanEye();
+		rightEye = new HumanEye();
+	}
+	
+	public class HumanEye extends Eye{
+		
+	}
+
+}
